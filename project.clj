98a18f54151659632326589deeb07c862ff3b36c
Christoph Frick <cf@ofnir.net>
2021-07-05 10:10:46 +0200

    Update versions and enable building native image

--- project.clj
+++ project.clj
@@ -3,12 +3,20 @@
   :url "https://github.com/christoph-frick/baubotanik"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/test.check "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/test.check "1.1.0"]
                  [com.stuartsierra/dependency "1.0.0"]
-                 [expound "0.8.5"]
-                 [fmnoise/flow "4.0.0"]]
-  :main ^:skip-aot net.ofnir.baubotanik.main
+                 [expound "0.8.9"]
+                 [fmnoise/flow "4.1.0"]]
+  :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
+  :native-image {:name "baubotanik"
+                 :opts ["--verbose"
+                        "--initialize-at-build-time"
+                        "--report-unsupported-elements-at-runtime"
+                        "--no-server"
+                        "--no-fallback"]}
+  :main net.ofnir.baubotanik.main
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
   :global-vars {*warn-on-reflection* true}
+  :profiles {:uberjar {:aot :all
+                       :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}})
